796207c57a7209defdc241448146a0b40658faaa
Hugo Peek <hugo@fractal-farming.com>
2017-12-16 21:47:44 +0800

    Update pattern library

--- plugins/plLexiconMgr.php
+++ plugins/plLexiconMgr.php
@@ -1,11 +1,15 @@
 id: 19
 name: plLexiconMgr
 category: c_global
-plugincode: '$modx->controller->addLexiconTopic(''romanescobackyard:manager'');'
+plugincode: "$modx->controller->addLexiconTopic('romanescobackyard:manager');\n\n# Load custom CSS styles for ContentBlocks\n$url = $modx->getOption('base_url') . 'assets/components/romanescobackyard/css/contentblocks.css';\n$modx->regClientCss($url);"
 properties: 'a:0:{}'
-content: '$modx->controller->addLexiconTopic(''romanescobackyard:manager'');'
+content: "$modx->controller->addLexiconTopic('romanescobackyard:manager');\n\n# Load custom CSS styles for ContentBlocks\n$url = $modx->getOption('base_url') . 'assets/components/romanescobackyard/css/contentblocks.css';\n$modx->regClientCss($url);"
 
 -----
 
 
 $modx->controller->addLexiconTopic('romanescobackyard:manager');
+
+# Load custom CSS styles for ContentBlocks
+$url = $modx->getOption('base_url') . 'assets/components/romanescobackyard/css/contentblocks.css';
+$modx->regClientCss($url);
