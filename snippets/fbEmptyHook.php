773e070f016e6392e317a836244936105fe2fb9c
Hugo Peek <hugo@fractal-farming.com>
2017-12-20 19:39:33 +0800

    FormBlocks improvements
    - Enable editor to add custom (pre)hooks to forms
    - Add option to set form layout in Form CB itself

---
+++ snippets/fbEmptyHook.php
@@ -0,0 +1,20 @@
+id: 119
+name: fbEmptyHook
+category: f_fb_hooks
+snippet: "/**\n * \n * fbEmptyHook\n *\n * Dummy hook to trick FormIt into not throwing errors saying there are no hooks,\n * and to make the comma separation between hooks a little easier.\n */\n\n// Nothing to see here, move along..\nreturn true;"
+properties: 'a:0:{}'
+content: "/**\n * \n * fbEmptyHook\n *\n * Dummy hook to trick FormIt into not throwing errors saying there are no hooks,\n * and to make the comma separation between hooks a little easier.\n */\n\n// Nothing to see here, move along..\nreturn true;"
+
+-----
+
+
+/**
+ * 
+ * fbEmptyHook
+ *
+ * Dummy hook to trick FormIt into not throwing errors saying there are no hooks,
+ * and to make the comma separation between hooks a little easier.
+ */
+
+// Nothing to see here, move along..
+return true;
