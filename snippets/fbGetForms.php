df32b8547cddc7f7c3cf8e2deaba1cdfe4f053b0
Hugo Peek <hugo@fractal-farming.com>
2019-08-30 22:05:28 +0200

    Update pattern library

--- snippets/fbGetForms.php
+++ snippets/fbGetForms.php
@@ -1,14 +1,15 @@
 id: 57
 name: fbGetForms
 category: f_formblocks
-snippet: "$parentID = $modx->getOption('formblocks.container_id');\n\n$output = $modx->runSnippet('getResources', (array(\n    'parents' => $parentID,\n    'limit' => 99,\n    'showHidden' => 1,\n    'showUnpublished' => 1,\n    'tpl' => '@INLINE [[+pagetitle]]=[[+id]]',\n    'sortby' => 'menuindex',\n    'sortdir' => 'ASC',\n)));\n\nreturn $output;"
+snippet: "$context = $modx->getContext($modx->resource->get('context_key'));\n$parentID = $context->getOption('formblocks.container_id') ?? $modx->getOption('formblocks.container_id');\n\n$output = $modx->runSnippet('getResources', (array(\n    'parents' => $parentID,\n    'limit' => 99,\n    'showHidden' => 1,\n    'showUnpublished' => 1,\n    'tpl' => '@INLINE [[+pagetitle]]=[[+id]]',\n    'sortby' => 'menuindex',\n    'sortdir' => 'ASC',\n)));\n\nreturn $output;"
 properties: 'a:0:{}'
-content: "$parentID = $modx->getOption('formblocks.container_id');\n\n$output = $modx->runSnippet('getResources', (array(\n    'parents' => $parentID,\n    'limit' => 99,\n    'showHidden' => 1,\n    'showUnpublished' => 1,\n    'tpl' => '@INLINE [[+pagetitle]]=[[+id]]',\n    'sortby' => 'menuindex',\n    'sortdir' => 'ASC',\n)));\n\nreturn $output;"
+content: "$context = $modx->getContext($modx->resource->get('context_key'));\n$parentID = $context->getOption('formblocks.container_id') ?? $modx->getOption('formblocks.container_id');\n\n$output = $modx->runSnippet('getResources', (array(\n    'parents' => $parentID,\n    'limit' => 99,\n    'showHidden' => 1,\n    'showUnpublished' => 1,\n    'tpl' => '@INLINE [[+pagetitle]]=[[+id]]',\n    'sortby' => 'menuindex',\n    'sortdir' => 'ASC',\n)));\n\nreturn $output;"
 
 -----
 
 
-$parentID = $modx->getOption('formblocks.container_id');
+$context = $modx->getContext($modx->resource->get('context_key'));
+$parentID = $context->getOption('formblocks.container_id') ?? $modx->getOption('formblocks.container_id');
 
 $output = $modx->runSnippet('getResources', (array(
     'parents' => $parentID,
--- snippets/resourceTVInputOptions.php
+++ snippets/resourceTVInputOptions.php
@@ -10,21 +10,42 @@ content: "/**\n * @todo: Is this snippet used anywhere?\n */\n\n$parents = $modx
 
 
 /**
+ * resourceTVInputOptions
+ *
  * @todo: Is this snippet used anywhere?
+ *
+ * Grab list of resources from given parent. Parent ID can be provided directly
+ * (with &parents) or via a context or system setting (&key).
+ *
+ * For use in ContentBlocks settings, add &tplMode=`cb`.
  */
 
-$parents = $modx->getOption('parents',$scriptProperties,true);
+$key = $modx->getOption('key',$scriptProperties,null);
+$context = $modx->getContext($modx->resource->get('context_key'));
+$parents = $context->getOption($key) ?? $modx->getOption('parents',$scriptProperties,$key);
+
+$tpl = $modx->getOption('tpl',$scriptProperties,'');
+$tplMode = $modx->getOption('tplMode',$scriptProperties,'tv');
+$separator = $modx->getOption('outputSeparator',$scriptProperties,null);
+
+// Set appropriate template if no custom tpl is defined
+if (!$tpl && $tplMode == 'cb') {
+    $tpl = '@INLINE [[+pagetitle]]=[[+id]]';
+}
+if (!$tpl && $tplMode == 'tv') {
+    $tpl = '@INLINE [[+pagetitle]]==[[+id]]';
+    $separator = '||';
+}
 
 $output = $modx->runSnippet('getResources', (array(
     'parents' => $parents,
+    'limit' => 99,
     'showHidden' => 1,
     'showUnpublished' => 1,
-    'limit' => '99',
-    'sortBy' => 'menuindex',
-    'sortDir' => 'ASC',
-    'tpl' => '@INLINE [[+pagetitle]]==[[+id]]',
-    'outputSeparator' => '||'
-)
-));
+    'tpl' => $tpl,
+    'sortby' => 'menuindex',
+    'sortdir' => 'ASC',
+    'outputSeparator' => $separator,
+)));
 
 return $output;
