a3e1742bdd09d4c1663ceed32431428100eaf6fc
Hugo Peek <hugo@fractal-farming.com>
2019-10-10 18:54:17 +0800

    Update pattern library

--- plugins/MarkdownMimeType.php
+++ plugins/MarkdownMimeType.php
@@ -1,9 +1,9 @@
 id: 32
 name: MarkdownMimeType
 category: c_content
-plugincode: "/**\n * MarkdownMimeType\n *\n * Retain original Markdown markup by setting the proper MIME type for a\n * Markdown resource. Set the MIME type back to HTML when viewing the resource\n * in the browser, to prevent the page from being downloaded as file.\n *\n * For rendering Markdown as HTML, install the Markdown extra from modstore.pro:\n * https://modstore.pro/packages/content/markdown\n *\n * Escape backticks in your template like this:\n *\n * [[Markdown? &input=`[[*content:replace=``==&#96;`]]`]]\n */\n\nswitch ($modx->event->name) {\n    // Set content type to Markdown when resource has a markdown template\n    case 'OnBeforeDocFormSave':\n        $template = $modx->getObject('modTemplate', array('id'=>$resource->get('template')));\n\n        if (!is_object($template)) {\n            break;\n        }\n\n        // Any template with 'Markdown' in its name qualifies\n        // Note to self: you need to revert the content type manually if you assign a non-markdown template again\n        if (stripos($template->get('templatename'), 'markdown') !== false) {\n            $resource->set('contentType', 'text/x-markdown');\n            $resource->set('content_type', '11');\n\n            // Also disable any active text editor\n            $resource->set('richtext', 0);\n        }\n\n        break;\n\n    // Use HTML mime type when viewed as a web page\n    // Inspired by: https://github.com/GoldCoastMedia/modx-xhtml-mime-switch\n    case 'OnWebPagePrerender':\n        $resource = &$modx->resource;\n\n        // Header content types\n        $header = (object) array(\n            'markdown'  => 'text/x-markdown',\n            'html' => 'text/html',\n        );\n\n        // Get the document type\n        $markdown = ($resource->get('contentType') === $header->markdown) ? true : false;\n\n        // Switch back to HTML\n        if ($markdown) {\n            $resource->ContentType->set('mime_type', $header->html);\n        }\n\n        break;\n}"
+plugincode: "/**\n * MarkdownMimeType\n *\n * Retain original Markdown markup by setting the proper MIME type for a\n * Markdown resource. Set the MIME type back to HTML when viewing the resource\n * in the browser, to prevent the page from being downloaded as file.\n *\n * In addition, HtmlPageDom is used to fix image URLs and prevent them from\n * overflowing their container.\n *\n * For rendering Markdown as HTML, install the Markdown extra from modstore.pro:\n * https://modstore.pro/packages/content/markdown\n *\n * Process markdown in your template like this:\n *\n * [[*content:Markdown]]\n */\n\n$corePath = $modx->getOption('htmlpagedom.core_path', null, $modx->getOption('core_path') . 'components/htmlpagedom/');\n\nif (!class_exists('\\Wa72\\HtmlPageDom\\HtmlPageCrawler')) {\n    require $corePath . 'vendor/autoload.php';\n}\n\nuse \\Wa72\\HtmlPageDom\\HtmlPageCrawler;\n\nswitch ($modx->event->name) {\n    // Set content type to Markdown when resource has a markdown template\n    case 'OnBeforeDocFormSave':\n        $template = $modx->getObject('modTemplate', array('id'=>$resource->get('template')));\n\n        if (!is_object($template)) {\n            break;\n        }\n\n        // Any template with 'Markdown' in its name qualifies\n        // Note to self: you need to revert the content type manually if you assign a non-markdown template again\n        if (stripos($template->get('templatename'), 'markdown') !== false) {\n            $resource->set('contentType', 'text/x-markdown');\n            $resource->set('content_type', '11');\n\n            // Also disable any active text editor\n            $resource->set('richtext', 0);\n        }\n\n        break;\n\n    // Use HTML mime type when viewed as a web page\n    // Inspired by: https://github.com/GoldCoastMedia/modx-xhtml-mime-switch\n    case 'OnWebPagePrerender':\n        $resource = &$modx->resource;\n\n        // Header content types\n        $header = (object) array(\n            'markdown'  => 'text/x-markdown',\n            'html' => 'text/html',\n        );\n\n        // Get the document type\n        $markdown = ($resource->get('contentType') === $header->markdown) ? true : false;\n\n        // Switch back to HTML\n        if ($markdown) {\n            $resource->ContentType->set('mime_type', $header->html);\n        } else {\n            break;\n        }\n\n        // Process output with HtmlPageDom\n        $output = &$resource->_output;\n        $dom = new HtmlPageCrawler($output);\n\n        // Fix image URLs and display size\n        $dom->filter('img')\n            ->each(function (HtmlPageCrawler $image) {\n                $src = $image->getAttribute('src');\n                $image\n                    ->setAttribute('src', 'notes/' . $src)\n                    ->addClass('ui image')\n                ;\n            })\n        ;\n\n        $output = $dom->saveHTML();\n\n        break;\n}"
 properties: 'a:0:{}'
-content: "/**\n * MarkdownMimeType\n *\n * Retain original Markdown markup by setting the proper MIME type for a\n * Markdown resource. Set the MIME type back to HTML when viewing the resource\n * in the browser, to prevent the page from being downloaded as file.\n *\n * For rendering Markdown as HTML, install the Markdown extra from modstore.pro:\n * https://modstore.pro/packages/content/markdown\n *\n * Escape backticks in your template like this:\n *\n * [[Markdown? &input=`[[*content:replace=``==&#96;`]]`]]\n */\n\nswitch ($modx->event->name) {\n    // Set content type to Markdown when resource has a markdown template\n    case 'OnBeforeDocFormSave':\n        $template = $modx->getObject('modTemplate', array('id'=>$resource->get('template')));\n\n        if (!is_object($template)) {\n            break;\n        }\n\n        // Any template with 'Markdown' in its name qualifies\n        // Note to self: you need to revert the content type manually if you assign a non-markdown template again\n        if (stripos($template->get('templatename'), 'markdown') !== false) {\n            $resource->set('contentType', 'text/x-markdown');\n            $resource->set('content_type', '11');\n\n            // Also disable any active text editor\n            $resource->set('richtext', 0);\n        }\n\n        break;\n\n    // Use HTML mime type when viewed as a web page\n    // Inspired by: https://github.com/GoldCoastMedia/modx-xhtml-mime-switch\n    case 'OnWebPagePrerender':\n        $resource = &$modx->resource;\n\n        // Header content types\n        $header = (object) array(\n            'markdown'  => 'text/x-markdown',\n            'html' => 'text/html',\n        );\n\n        // Get the document type\n        $markdown = ($resource->get('contentType') === $header->markdown) ? true : false;\n\n        // Switch back to HTML\n        if ($markdown) {\n            $resource->ContentType->set('mime_type', $header->html);\n        }\n\n        break;\n}"
+content: "/**\n * MarkdownMimeType\n *\n * Retain original Markdown markup by setting the proper MIME type for a\n * Markdown resource. Set the MIME type back to HTML when viewing the resource\n * in the browser, to prevent the page from being downloaded as file.\n *\n * In addition, HtmlPageDom is used to fix image URLs and prevent them from\n * overflowing their container.\n *\n * For rendering Markdown as HTML, install the Markdown extra from modstore.pro:\n * https://modstore.pro/packages/content/markdown\n *\n * Process markdown in your template like this:\n *\n * [[*content:Markdown]]\n */\n\n$corePath = $modx->getOption('htmlpagedom.core_path', null, $modx->getOption('core_path') . 'components/htmlpagedom/');\n\nif (!class_exists('\\Wa72\\HtmlPageDom\\HtmlPageCrawler')) {\n    require $corePath . 'vendor/autoload.php';\n}\n\nuse \\Wa72\\HtmlPageDom\\HtmlPageCrawler;\n\nswitch ($modx->event->name) {\n    // Set content type to Markdown when resource has a markdown template\n    case 'OnBeforeDocFormSave':\n        $template = $modx->getObject('modTemplate', array('id'=>$resource->get('template')));\n\n        if (!is_object($template)) {\n            break;\n        }\n\n        // Any template with 'Markdown' in its name qualifies\n        // Note to self: you need to revert the content type manually if you assign a non-markdown template again\n        if (stripos($template->get('templatename'), 'markdown') !== false) {\n            $resource->set('contentType', 'text/x-markdown');\n            $resource->set('content_type', '11');\n\n            // Also disable any active text editor\n            $resource->set('richtext', 0);\n        }\n\n        break;\n\n    // Use HTML mime type when viewed as a web page\n    // Inspired by: https://github.com/GoldCoastMedia/modx-xhtml-mime-switch\n    case 'OnWebPagePrerender':\n        $resource = &$modx->resource;\n\n        // Header content types\n        $header = (object) array(\n            'markdown'  => 'text/x-markdown',\n            'html' => 'text/html',\n        );\n\n        // Get the document type\n        $markdown = ($resource->get('contentType') === $header->markdown) ? true : false;\n\n        // Switch back to HTML\n        if ($markdown) {\n            $resource->ContentType->set('mime_type', $header->html);\n        } else {\n            break;\n        }\n\n        // Process output with HtmlPageDom\n        $output = &$resource->_output;\n        $dom = new HtmlPageCrawler($output);\n\n        // Fix image URLs and display size\n        $dom->filter('img')\n            ->each(function (HtmlPageCrawler $image) {\n                $src = $image->getAttribute('src');\n                $image\n                    ->setAttribute('src', 'notes/' . $src)\n                    ->addClass('ui image')\n                ;\n            })\n        ;\n\n        $output = $dom->saveHTML();\n\n        break;\n}"
 
 -----
 
@@ -15,13 +15,24 @@ content: "/**\n * MarkdownMimeType\n *\n * Retain original Markdown markup by se
  * Markdown resource. Set the MIME type back to HTML when viewing the resource
  * in the browser, to prevent the page from being downloaded as file.
  *
+ * In addition, HtmlPageDom is used to fix image URLs and prevent them from
+ * overflowing their container.
+ *
  * For rendering Markdown as HTML, install the Markdown extra from modstore.pro:
  * https://modstore.pro/packages/content/markdown
  *
- * Escape backticks in your template like this:
+ * Process markdown in your template like this:
  *
- * [[Markdown? &input=`[[*content:replace=``==&#96;`]]`]]
+ * [[*content:Markdown]]
  */
+
+$corePath = $modx->getOption('htmlpagedom.core_path', null, $modx->getOption('core_path') . 'components/htmlpagedom/');
+
+if (!class_exists('\Wa72\HtmlPageDom\HtmlPageCrawler')) {
+    require $corePath . 'vendor/autoload.php';
+}
+
+use \Wa72\HtmlPageDom\HtmlPageCrawler;
 
 switch ($modx->event->name) {
     // Set content type to Markdown when resource has a markdown template
@@ -61,7 +72,26 @@ switch ($modx->event->name) {
         // Switch back to HTML
         if ($markdown) {
             $resource->ContentType->set('mime_type', $header->html);
+        } else {
+            break;
         }
+
+        // Process output with HtmlPageDom
+        $output = &$resource->_output;
+        $dom = new HtmlPageCrawler($output);
+
+        // Fix image URLs and display size
+        $dom->filter('img')
+            ->each(function (HtmlPageCrawler $image) {
+                $src = $image->getAttribute('src');
+                $image
+                    ->setAttribute('src', 'notes/' . $src)
+                    ->addClass('ui image')
+                ;
+            })
+        ;
+
+        $output = $dom->saveHTML();
 
         break;
 }
---
+++ snippets/generateStaticFile.php
@@ -0,0 +1,43 @@
+id: 141
+name: generateStaticFile
+description: 'Create a physical HTML file of a resource in the designated location. This is a utility snippet. Place it in the content somewhere and visit that page in the browser to generate the file.'
+category: f_framework
+snippet: "/**\n * generateStaticFile\n *\n * Create a physical HTML file of a resource in the designated location.\n *\n * Usage: this is a utility snippet. Place it in the content somewhere and visit\n * that page in the browser to generate the file.\n *\n * NB! Won't work if the snippet is on the page you want to export!\n *\n * @todo: convert to plugin, support multiple files, add ability to change site_url.\n */\n\n$file = $modx->getOption('file', $scriptProperties, '');\n$resourceID = $modx->getOption('id', $scriptProperties, '');\n\n$pathInfo = pathinfo($file);\n$path = $pathInfo['dirname'];\n\nif (!file_exists($path)) {\n    mkdir($path, 0755, true);\n}\n\nif ($file) {\n    // The resource needs to be processed by MODX first.\n    // The easiest way to get the result is through the browser.\n    $content = file_get_contents($modx->makeUrl($resourceID,'','','full')) . PHP_EOL;\n\n    file_put_contents($file, $content);\n}\n\nreturn;"
+properties: 'a:0:{}'
+content: "/**\n * generateStaticFile\n *\n * Create a physical HTML file of a resource in the designated location.\n *\n * Usage: this is a utility snippet. Place it in the content somewhere and visit\n * that page in the browser to generate the file.\n *\n * NB! Won't work if the snippet is on the page you want to export!\n *\n * @todo: convert to plugin, support multiple files, add ability to change site_url.\n */\n\n$file = $modx->getOption('file', $scriptProperties, '');\n$resourceID = $modx->getOption('id', $scriptProperties, '');\n\n$pathInfo = pathinfo($file);\n$path = $pathInfo['dirname'];\n\nif (!file_exists($path)) {\n    mkdir($path, 0755, true);\n}\n\nif ($file) {\n    // The resource needs to be processed by MODX first.\n    // The easiest way to get the result is through the browser.\n    $content = file_get_contents($modx->makeUrl($resourceID,'','','full')) . PHP_EOL;\n\n    file_put_contents($file, $content);\n}\n\nreturn;"
+
+-----
+
+
+/**
+ * generateStaticFile
+ *
+ * Create a physical HTML file of a resource in the designated location.
+ *
+ * Usage: this is a utility snippet. Place it in the content somewhere and visit
+ * that page in the browser to generate the file.
+ *
+ * NB! Won't work if the snippet is on the page you want to export!
+ *
+ * @todo: convert to plugin, support multiple files, add ability to change site_url.
+ */
+
+$file = $modx->getOption('file', $scriptProperties, '');
+$resourceID = $modx->getOption('id', $scriptProperties, '');
+
+$pathInfo = pathinfo($file);
+$path = $pathInfo['dirname'];
+
+if (!file_exists($path)) {
+    mkdir($path, 0755, true);
+}
+
+if ($file) {
+    // The resource needs to be processed by MODX first.
+    // The easiest way to get the result is through the browser.
+    $content = file_get_contents($modx->makeUrl($resourceID,'','','full')) . PHP_EOL;
+
+    file_put_contents($file, $content);
+}
+
+return;
