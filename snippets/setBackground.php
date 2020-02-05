977f94087813123f7aab608b28251565dadaccc3
Hugo Peek <hugo@fractal-farming.com>
2020-02-05 20:27:08 +0800

    Update pattern library

--- snippets/setBackground.php
+++ snippets/setBackground.php
@@ -2,9 +2,9 @@ id: 142
 name: setBackground
 description: 'Output the necessary class names for the applied Global Background.'
 category: f_framework
-snippet: "/**\n * setBackground\n *\n * Set the appropriate classes for an applied Global Background.\n * For use in CB layout templates.\n *\n * Backwards compatible with the old, classname-based approach.\n */\n\n$background = $modx->getOption('background', $scriptProperties, $input);\n$cbField = $modx->getOption('romanesco.cb_field_background_id', $scriptProperties, '');\n\n// Numeric value means it's a resource-based background\nif (is_numeric($background)) {\n    $query = $modx->newQuery('modResource', $background);\n    $query->select('alias');\n    $alias = $modx->getValue($query->prepare());\n\n    // Get inverted value from CB settings\n    $inverted = $modx->runSnippet('cbGetFieldContent',array(\n        'resource' => $background,\n        'field' => $cbField,\n        'fieldSettingFilter' => 'inverted==1',\n        'returnAsJSON' => 1,\n    ));\n\n    // Default output is []\n    if (strlen($inverted) > 2) {\n        $inverted = ' inverted';\n    } else {\n        $inverted = '';\n    }\n\n    return $alias . $inverted . ' background';\n}\n\n// For backwards compatibility, return the old classes by default\nreturn $background;"
+snippet: "/**\n * setBackground\n *\n * Set the appropriate classes for an applied Global Background.\n * For use in CB layout templates.\n *\n * Backwards compatible with the old, classname-based approach.\n */\n\n$background = $modx->getOption('background', $scriptProperties, $input);\n$cbField = $modx->getOption('romanesco.cb_field_background_id', $scriptProperties, '');\n\n// Convert system default value\nif ($background == 'default') {\n    $background = $modx->getObject('cgSetting', array('key' => 'layout_background_default'))->get('value');\n}\n\n// Numeric value means it's a resource-based global background\nif (is_numeric($background)) {\n    $query = $modx->newQuery('modResource', $background);\n    $query->select('alias');\n    $alias = $modx->getValue($query->prepare());\n\n    // Get inverted value from CB settings\n    $inverted = $modx->runSnippet('cbGetFieldContent',array(\n        'resource' => $background,\n        'field' => $cbField,\n        'fieldSettingFilter' => 'inverted==1',\n        'returnAsJSON' => 1,\n    ));\n\n    // Default output is []\n    if (strlen($inverted) > 2) {\n        $inverted = ' inverted';\n    } else {\n        $inverted = '';\n    }\n\n    $background = $alias . $inverted . ' background';\n}\n\nreturn $background;"
 properties: 'a:0:{}'
-content: "/**\n * setBackground\n *\n * Set the appropriate classes for an applied Global Background.\n * For use in CB layout templates.\n *\n * Backwards compatible with the old, classname-based approach.\n */\n\n$background = $modx->getOption('background', $scriptProperties, $input);\n$cbField = $modx->getOption('romanesco.cb_field_background_id', $scriptProperties, '');\n\n// Numeric value means it's a resource-based background\nif (is_numeric($background)) {\n    $query = $modx->newQuery('modResource', $background);\n    $query->select('alias');\n    $alias = $modx->getValue($query->prepare());\n\n    // Get inverted value from CB settings\n    $inverted = $modx->runSnippet('cbGetFieldContent',array(\n        'resource' => $background,\n        'field' => $cbField,\n        'fieldSettingFilter' => 'inverted==1',\n        'returnAsJSON' => 1,\n    ));\n\n    // Default output is []\n    if (strlen($inverted) > 2) {\n        $inverted = ' inverted';\n    } else {\n        $inverted = '';\n    }\n\n    return $alias . $inverted . ' background';\n}\n\n// For backwards compatibility, return the old classes by default\nreturn $background;"
+content: "/**\n * setBackground\n *\n * Set the appropriate classes for an applied Global Background.\n * For use in CB layout templates.\n *\n * Backwards compatible with the old, classname-based approach.\n */\n\n$background = $modx->getOption('background', $scriptProperties, $input);\n$cbField = $modx->getOption('romanesco.cb_field_background_id', $scriptProperties, '');\n\n// Convert system default value\nif ($background == 'default') {\n    $background = $modx->getObject('cgSetting', array('key' => 'layout_background_default'))->get('value');\n}\n\n// Numeric value means it's a resource-based global background\nif (is_numeric($background)) {\n    $query = $modx->newQuery('modResource', $background);\n    $query->select('alias');\n    $alias = $modx->getValue($query->prepare());\n\n    // Get inverted value from CB settings\n    $inverted = $modx->runSnippet('cbGetFieldContent',array(\n        'resource' => $background,\n        'field' => $cbField,\n        'fieldSettingFilter' => 'inverted==1',\n        'returnAsJSON' => 1,\n    ));\n\n    // Default output is []\n    if (strlen($inverted) > 2) {\n        $inverted = ' inverted';\n    } else {\n        $inverted = '';\n    }\n\n    $background = $alias . $inverted . ' background';\n}\n\nreturn $background;"
 
 -----
 
@@ -21,7 +21,12 @@ content: "/**\n * setBackground\n *\n * Set the appropriate classes for an appli
 $background = $modx->getOption('background', $scriptProperties, $input);
 $cbField = $modx->getOption('romanesco.cb_field_background_id', $scriptProperties, '');
 
-// Numeric value means it's a resource-based background
+// Convert system default value
+if ($background == 'default') {
+    $background = $modx->getObject('cgSetting', array('key' => 'layout_background_default'))->get('value');
+}
+
+// Numeric value means it's a resource-based global background
 if (is_numeric($background)) {
     $query = $modx->newQuery('modResource', $background);
     $query->select('alias');
@@ -42,8 +47,7 @@ if (is_numeric($background)) {
         $inverted = '';
     }
 
-    return $alias . $inverted . ' background';
+    $background = $alias . $inverted . ' background';
 }
 
-// For backwards compatibility, return the old classes by default
 return $background;
