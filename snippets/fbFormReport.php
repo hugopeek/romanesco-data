0f845a457209818b16c170b0f396d3684bcb2a1b
Hugo Peek <hugo@fractal-farming.com>
2020-03-28 18:20:40 +0800

    Update pattern library

--- snippets/fbFormReport.php
+++ snippets/fbFormReport.php
@@ -2,9 +2,9 @@ id: 144
 name: fbFormReport
 description: 'Generates a report from submitted field values. Primarily for emails, but you can also use this snippet to template other kinds of functionality (confirmation pages, multi page forms..).'
 category: f_formblocks
-snippet: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix\n * &id: Resource ID of the resource where the form is being used\n */\n\nfunction getFields(&$modx, $data, $prefix) {\n    $result = '';\n\n    foreach($data as $key => $value) {\n        if(!is_array($value)) {\n            continue;\n        }\n\n        if(isset($value['field'])) {\n            $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n            continue;\n        }\n\n        $result .= getFields($modx, $value, $prefix);\n    }\n\n    return $result;\n}\n\n$output = '';\n\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$id = $modx->getOption('id', $scriptProperties, $modx->resource->get('id'));\n$resource = $modx->getObject('modResource', $id);\n\n$cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n\n$output .= getFields($modx, $cbData, $tplPrefix);\n\nreturn $output;"
+snippet: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix.\n * &formID: Resource ID of the form. Can be a comma-separated list also, for\n *  processing multi-page forms.\n */\n\nif (!function_exists('getFields')) {\n    function getFields(&$modx, $data, $prefix, $id) {\n        $result = '';\n\n        foreach($data as $key => $value) {\n            if(!is_array($value)) {\n                continue;\n            }\n\n            if(isset($value['field'])) {\n                $value['settings']['id'] = $id;\n                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n                continue;\n            }\n\n            $result .= getFields($modx, $value, $prefix, $id);\n        }\n\n        return $result;\n    }\n}\n\n$output = '';\n\n$formID = $modx->getOption('formID', $scriptProperties, $modx->resource->get('id'));\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');\n\n$forms = explode(',',$formID);\n\nforeach ($forms as $formID) {\n    $resource = $modx->getObject('modResource', $formID);\n    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n\n    // Only add header if there are multiple forms and a tpl chunk present\n    if ($forms[1] && $tplSectionHeader) {\n        $output .= $modx->getChunk($tplSectionHeader, array(\"title\" => $resource->get('pagetitle')));\n    }\n\n    $output .= getFields($modx, $cbData, $tplPrefix, $formID);\n}\n\nreturn $output;"
 properties: 'a:0:{}'
-content: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix\n * &id: Resource ID of the resource where the form is being used\n */\n\nfunction getFields(&$modx, $data, $prefix) {\n    $result = '';\n\n    foreach($data as $key => $value) {\n        if(!is_array($value)) {\n            continue;\n        }\n\n        if(isset($value['field'])) {\n            $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n            continue;\n        }\n\n        $result .= getFields($modx, $value, $prefix);\n    }\n\n    return $result;\n}\n\n$output = '';\n\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$id = $modx->getOption('id', $scriptProperties, $modx->resource->get('id'));\n$resource = $modx->getObject('modResource', $id);\n\n$cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n\n$output .= getFields($modx, $cbData, $tplPrefix);\n\nreturn $output;"
+content: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix.\n * &formID: Resource ID of the form. Can be a comma-separated list also, for\n *  processing multi-page forms.\n */\n\nif (!function_exists('getFields')) {\n    function getFields(&$modx, $data, $prefix, $id) {\n        $result = '';\n\n        foreach($data as $key => $value) {\n            if(!is_array($value)) {\n                continue;\n            }\n\n            if(isset($value['field'])) {\n                $value['settings']['id'] = $id;\n                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n                continue;\n            }\n\n            $result .= getFields($modx, $value, $prefix, $id);\n        }\n\n        return $result;\n    }\n}\n\n$output = '';\n\n$formID = $modx->getOption('formID', $scriptProperties, $modx->resource->get('id'));\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');\n\n$forms = explode(',',$formID);\n\nforeach ($forms as $formID) {\n    $resource = $modx->getObject('modResource', $formID);\n    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n\n    // Only add header if there are multiple forms and a tpl chunk present\n    if ($forms[1] && $tplSectionHeader) {\n        $output .= $modx->getChunk($tplSectionHeader, array(\"title\" => $resource->get('pagetitle')));\n    }\n\n    $output .= getFields($modx, $cbData, $tplPrefix, $formID);\n}\n\nreturn $output;"
 
 -----
 
@@ -19,37 +19,51 @@ content: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted
  * @author Jsewill
  * @version 1.0
  *
- * &tplPrefix: Template chunk name prefix
- * &id: Resource ID of the resource where the form is being used
+ * &tplPrefix: Template chunk name prefix.
+ * &formID: Resource ID of the form. Can be a comma-separated list also, for
+ *  processing multi-page forms.
  */
 
-function getFields(&$modx, $data, $prefix) {
-    $result = '';
+if (!function_exists('getFields')) {
+    function getFields(&$modx, $data, $prefix, $id) {
+        $result = '';
 
-    foreach($data as $key => $value) {
-        if(!is_array($value)) {
-            continue;
+        foreach($data as $key => $value) {
+            if(!is_array($value)) {
+                continue;
+            }
+
+            if(isset($value['field'])) {
+                $value['settings']['id'] = $id;
+                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);
+                continue;
+            }
+
+            $result .= getFields($modx, $value, $prefix, $id);
         }
 
-        if(isset($value['field'])) {
-            $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);
-            continue;
-        }
-
-        $result .= getFields($modx, $value, $prefix);
+        return $result;
     }
-
-    return $result;
 }
 
 $output = '';
 
+$formID = $modx->getOption('formID', $scriptProperties, $modx->resource->get('id'));
 $tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');
-$id = $modx->getOption('id', $scriptProperties, $modx->resource->get('id'));
-$resource = $modx->getObject('modResource', $id);
+$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');
 
-$cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);
+$forms = explode(',',$formID);
 
-$output .= getFields($modx, $cbData, $tplPrefix);
+foreach ($forms as $formID) {
+    $resource = $modx->getObject('modResource', $formID);
+    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);
+
+    // Only add header if there are multiple forms and a tpl chunk present
+    if ($forms[1] && $tplSectionHeader) {
+        $output .= $modx->getChunk($tplSectionHeader, array("title" => $resource->get('pagetitle')));
+    }
+
+    $output .= getFields($modx, $cbData, $tplPrefix, $formID);
+}
 
 return $output;
