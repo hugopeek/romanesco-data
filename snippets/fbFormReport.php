535395eb459bec7f0ab1ba431f23b21e06945226
Hugo Peek <hugo@fractal-farming.com>
2020-04-14 09:51:21 +0800

    Update pattern library

--- snippets/fbFormReport.php
+++ snippets/fbFormReport.php
@@ -2,9 +2,9 @@ id: 144
 name: fbFormReport
 description: 'Generates a report from submitted field values. Primarily for emails, but you can also use this snippet to template other kinds of functionality (confirmation pages, multi page forms..).'
 category: f_formblocks
-snippet: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix.\n * &formID: Resource ID of the form. Can be a comma-separated list also, for\n *  processing multi-page forms.\n */\n\nif (!function_exists('getFields')) {\n    function getFields(&$modx, $data, $prefix, $id) {\n        $result = '';\n\n        foreach($data as $key => $value) {\n            if(!is_array($value)) {\n                continue;\n            }\n\n            if(isset($value['field'])) {\n                $value['settings']['id'] = $id;\n                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n                continue;\n            }\n\n            $result .= getFields($modx, $value, $prefix, $id);\n        }\n\n        return $result;\n    }\n}\n\n$output = '';\n\n$formID = $modx->getOption('formID', $scriptProperties, $modx->resource->get('id'));\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');\n\n$forms = explode(',',$formID);\n\nforeach ($forms as $formID) {\n    $resource = $modx->getObject('modResource', $formID);\n    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n\n    // Only add header if there are multiple forms and a tpl chunk present\n    if ($forms[1] && $tplSectionHeader) {\n        $output .= $modx->getChunk($tplSectionHeader, array(\"title\" => $resource->get('pagetitle')));\n    }\n\n    $output .= getFields($modx, $cbData, $tplPrefix, $formID);\n}\n\nreturn $output;"
+snippet: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix.\n * &formID: Resource ID of the form. Can be a comma-separated list also, for\n *  processing multi-page forms.\n */\n\nif (!function_exists('getFields')) {\n    function getFields(&$modx, $data, $prefix, $id) {\n        $result = '';\n\n        foreach($data as $key => $value) {\n            if(!is_array($value)) {\n                continue;\n            }\n\n            if(isset($value['field'])) {\n                $value['settings']['id'] = $id;\n                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n                continue;\n            }\n\n            $result .= getFields($modx, $value, $prefix, $id);\n        }\n\n        return $result;\n    }\n}\n\n$output = array();\n\n$formID = $modx->getOption('formID', $scriptProperties, '');\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');\n\nif (!$formID) return '';\n$forms = explode(',',$formID);\n\nforeach ($forms as $formID) {\n    $resource = $modx->getObject('modResource', $formID);\n    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n    $result = '';\n\n    // Only add header if there are multiple forms and a tpl chunk present\n    if ($forms[1] && $tplSectionHeader) {\n        $result .= $modx->getChunk($tplSectionHeader, array(\"title\" => $resource->get('pagetitle')));\n    }\n\n    $result .= getFields($modx, $cbData, $tplPrefix, $formID);\n\n    $output[] = $result;\n}\n\nreturn implode(array_reverse($output));"
 properties: 'a:0:{}'
-content: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix.\n * &formID: Resource ID of the form. Can be a comma-separated list also, for\n *  processing multi-page forms.\n */\n\nif (!function_exists('getFields')) {\n    function getFields(&$modx, $data, $prefix, $id) {\n        $result = '';\n\n        foreach($data as $key => $value) {\n            if(!is_array($value)) {\n                continue;\n            }\n\n            if(isset($value['field'])) {\n                $value['settings']['id'] = $id;\n                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n                continue;\n            }\n\n            $result .= getFields($modx, $value, $prefix, $id);\n        }\n\n        return $result;\n    }\n}\n\n$output = '';\n\n$formID = $modx->getOption('formID', $scriptProperties, $modx->resource->get('id'));\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');\n\n$forms = explode(',',$formID);\n\nforeach ($forms as $formID) {\n    $resource = $modx->getObject('modResource', $formID);\n    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n\n    // Only add header if there are multiple forms and a tpl chunk present\n    if ($forms[1] && $tplSectionHeader) {\n        $output .= $modx->getChunk($tplSectionHeader, array(\"title\" => $resource->get('pagetitle')));\n    }\n\n    $output .= getFields($modx, $cbData, $tplPrefix, $formID);\n}\n\nreturn $output;"
+content: "/**\n * fbFormReport Snippet\n *\n * Generates a report from submitted field values. Primarily used in email\n * responders of course, but you can also use this snippet to template other\n * kinds of functionality (confirmation pages, multi page forms..).\n *\n * @author Jsewill\n * @version 1.0\n *\n * &tplPrefix: Template chunk name prefix.\n * &formID: Resource ID of the form. Can be a comma-separated list also, for\n *  processing multi-page forms.\n */\n\nif (!function_exists('getFields')) {\n    function getFields(&$modx, $data, $prefix, $id) {\n        $result = '';\n\n        foreach($data as $key => $value) {\n            if(!is_array($value)) {\n                continue;\n            }\n\n            if(isset($value['field'])) {\n                $value['settings']['id'] = $id;\n                $result .= $modx->getChunk($prefix.$value['field'], $value['settings']);\n                continue;\n            }\n\n            $result .= getFields($modx, $value, $prefix, $id);\n        }\n\n        return $result;\n    }\n}\n\n$output = array();\n\n$formID = $modx->getOption('formID', $scriptProperties, '');\n$tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');\n$tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');\n\nif (!$formID) return '';\n$forms = explode(',',$formID);\n\nforeach ($forms as $formID) {\n    $resource = $modx->getObject('modResource', $formID);\n    $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);\n    $result = '';\n\n    // Only add header if there are multiple forms and a tpl chunk present\n    if ($forms[1] && $tplSectionHeader) {\n        $result .= $modx->getChunk($tplSectionHeader, array(\"title\" => $resource->get('pagetitle')));\n    }\n\n    $result .= getFields($modx, $cbData, $tplPrefix, $formID);\n\n    $output[] = $result;\n}\n\nreturn implode(array_reverse($output));"
 
 -----
 
@@ -46,24 +46,28 @@ if (!function_exists('getFields')) {
     }
 }
 
-$output = '';
+$output = array();
 
-$formID = $modx->getOption('formID', $scriptProperties, $modx->resource->get('id'));
+$formID = $modx->getOption('formID', $scriptProperties, '');
 $tplPrefix = $modx->getOption('tplPrefix', $scriptProperties, 'fbEmailRow_');
 $tplSectionHeader = $modx->getOption('tplSectionHeader', $scriptProperties, '');
 
+if (!$formID) return '';
 $forms = explode(',',$formID);
 
 foreach ($forms as $formID) {
     $resource = $modx->getObject('modResource', $formID);
     $cbData = json_decode($resource->getProperty('content', 'contentblocks'), true);
+    $result = '';
 
     // Only add header if there are multiple forms and a tpl chunk present
     if ($forms[1] && $tplSectionHeader) {
-        $output .= $modx->getChunk($tplSectionHeader, array("title" => $resource->get('pagetitle')));
+        $result .= $modx->getChunk($tplSectionHeader, array("title" => $resource->get('pagetitle')));
     }
 
-    $output .= getFields($modx, $cbData, $tplPrefix, $formID);
+    $result .= getFields($modx, $cbData, $tplPrefix, $formID);
+
+    $output[] = $result;
 }
 
-return $output;
+return implode(array_reverse($output));
--- snippets/jsonGetValue.php
+++ snippets/jsonGetValue.php
@@ -2,12 +2,17 @@ id: 72
 name: jsonGetValue
 description: 'Get the value of a specific key from a JSON string.'
 category: f_json
-snippet: "$input = $modx->getOption('json', $scriptProperties, $input);\n$key = $modx->getOption('key', $scriptProperties, $options);\n$tpl = $modx->getOption('tpl', $scriptProperties, '');\n$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, ',');\n$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n// @todo: test if input is valid JSON, otherwise NULL is returned\n$input = utf8_encode($input);\n$array = json_decode($input, true);\n\n// Single result from flat array\nif ($array[$key]) {\n    $output = $array[$key];\n\n    if ($tpl) {\n        $output = $modx->getChunk($tpl, array(\n            'content' => $output\n        ));\n    }\n};\n\n// Multiple keys from multidimensional array\nif (is_array($array[0])) {\n    foreach ($array as $item) {\n        $output[] = $item[0][$key];\n\n        if ($tpl) {\n            $output[] = $modx->getChunk($tpl, array(\n                'content' => $output\n            ));\n        }\n    }\n\n    $output = implode(',',$output);\n}\n\n// Output either to placeholder, or directly\nif ($toPlaceholder) {\n    $modx->setPlaceholder($toPlaceholder, $output);\n    return '';\n}\n\nreturn $output;"
+snippet: "$corePath = $modx->getOption('romanescobackyard.core_path', null, $modx->getOption('core_path') . 'components/romanescobackyard/');\n$romanesco = $modx->getService('romanesco','Romanesco',$corePath . 'model/romanescobackyard/',array('core_path' => $corePath));\n\nif (!($romanesco instanceof Romanesco)) return;\n\n$input = $modx->getOption('json', $scriptProperties, $input);\n$key = $modx->getOption('key', $scriptProperties, $options);\n$tpl = $modx->getOption('tpl', $scriptProperties, '');\n$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, ',');\n$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n// @todo: test if input is valid JSON, otherwise NULL is returned\n$input = utf8_encode($input);\n$array = json_decode($input, true);\n\n// Flatten first level, since that's always the full JSON object itself\n$array = $array[0];\n\n// Single result from flat array\nif ($array[$key]) {\n    $output = $array[$key];\n\n    if ($tpl) {\n        $output = $modx->getChunk($tpl, array(\n            'content' => $output\n        ));\n    }\n};\n\n// Single key from multidimensional array\nif (is_array($array)) {\n    $output = $romanesco->recursiveArraySearch($array, $key);\n    \n    if ($tpl) {\n        $output = $modx->getChunk($tpl, array(\n            'content' => $output\n        ));\n    }\n\n    $output = implode($output);\n}\n\n// Output either to placeholder, or directly\nif ($toPlaceholder) {\n    $modx->setPlaceholder($toPlaceholder, $output);\n    return '';\n}\n\nreturn $output;"
 properties: 'a:0:{}'
-content: "$input = $modx->getOption('json', $scriptProperties, $input);\n$key = $modx->getOption('key', $scriptProperties, $options);\n$tpl = $modx->getOption('tpl', $scriptProperties, '');\n$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, ',');\n$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n// @todo: test if input is valid JSON, otherwise NULL is returned\n$input = utf8_encode($input);\n$array = json_decode($input, true);\n\n// Single result from flat array\nif ($array[$key]) {\n    $output = $array[$key];\n\n    if ($tpl) {\n        $output = $modx->getChunk($tpl, array(\n            'content' => $output\n        ));\n    }\n};\n\n// Multiple keys from multidimensional array\nif (is_array($array[0])) {\n    foreach ($array as $item) {\n        $output[] = $item[0][$key];\n\n        if ($tpl) {\n            $output[] = $modx->getChunk($tpl, array(\n                'content' => $output\n            ));\n        }\n    }\n\n    $output = implode(',',$output);\n}\n\n// Output either to placeholder, or directly\nif ($toPlaceholder) {\n    $modx->setPlaceholder($toPlaceholder, $output);\n    return '';\n}\n\nreturn $output;"
+content: "$corePath = $modx->getOption('romanescobackyard.core_path', null, $modx->getOption('core_path') . 'components/romanescobackyard/');\n$romanesco = $modx->getService('romanesco','Romanesco',$corePath . 'model/romanescobackyard/',array('core_path' => $corePath));\n\nif (!($romanesco instanceof Romanesco)) return;\n\n$input = $modx->getOption('json', $scriptProperties, $input);\n$key = $modx->getOption('key', $scriptProperties, $options);\n$tpl = $modx->getOption('tpl', $scriptProperties, '');\n$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, ',');\n$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n// @todo: test if input is valid JSON, otherwise NULL is returned\n$input = utf8_encode($input);\n$array = json_decode($input, true);\n\n// Flatten first level, since that's always the full JSON object itself\n$array = $array[0];\n\n// Single result from flat array\nif ($array[$key]) {\n    $output = $array[$key];\n\n    if ($tpl) {\n        $output = $modx->getChunk($tpl, array(\n            'content' => $output\n        ));\n    }\n};\n\n// Single key from multidimensional array\nif (is_array($array)) {\n    $output = $romanesco->recursiveArraySearch($array, $key);\n    \n    if ($tpl) {\n        $output = $modx->getChunk($tpl, array(\n            'content' => $output\n        ));\n    }\n\n    $output = implode($output);\n}\n\n// Output either to placeholder, or directly\nif ($toPlaceholder) {\n    $modx->setPlaceholder($toPlaceholder, $output);\n    return '';\n}\n\nreturn $output;"
 
 -----
 
+
+$corePath = $modx->getOption('romanescobackyard.core_path', null, $modx->getOption('core_path') . 'components/romanescobackyard/');
+$romanesco = $modx->getService('romanesco','Romanesco',$corePath . 'model/romanescobackyard/',array('core_path' => $corePath));
+
+if (!($romanesco instanceof Romanesco)) return;
 
 $input = $modx->getOption('json', $scriptProperties, $input);
 $key = $modx->getOption('key', $scriptProperties, $options);
@@ -18,6 +23,9 @@ $toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
 // @todo: test if input is valid JSON, otherwise NULL is returned
 $input = utf8_encode($input);
 $array = json_decode($input, true);
+
+// Flatten first level, since that's always the full JSON object itself
+$array = $array[0];
 
 // Single result from flat array
 if ($array[$key]) {
@@ -30,19 +38,17 @@ if ($array[$key]) {
     }
 };
 
-// Multiple keys from multidimensional array
-if (is_array($array[0])) {
-    foreach ($array as $item) {
-        $output[] = $item[0][$key];
-
-        if ($tpl) {
-            $output[] = $modx->getChunk($tpl, array(
-                'content' => $output
-            ));
-        }
+// Single key from multidimensional array
+if (is_array($array)) {
+    $output = $romanesco->recursiveArraySearch($array, $key);
+    
+    if ($tpl) {
+        $output = $modx->getChunk($tpl, array(
+            'content' => $output
+        ));
     }
 
-    $output = implode(',',$output);
+    $output = implode($output);
 }
 
 // Output either to placeholder, or directly
