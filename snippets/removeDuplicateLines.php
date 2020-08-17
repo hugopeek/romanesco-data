f152bd3d9b34475e98d91d88a0425bbfa1746b64
Hugo Peek <hugo@fractal-farming.com>
2020-08-17 14:15:09 +0800

    Update pattern library

--- snippets/removeDuplicateLines.php
+++ snippets/removeDuplicateLines.php
@@ -2,9 +2,9 @@ id: 146
 name: removeDuplicateLines
 description: 'Scan input for duplicate lines and remove them from the output.'
 category: f_modifiers
-snippet: "$lines = $modx->getOption('input', $scriptProperties, $input);\n$file = $modx->getOption('file', $scriptProperties, '');\n\n// Check first if we're dealing with an external file\nif ($file) {\n    $lines = file_get_contents($file);\n}\n\n// Create an array of all lines inside the input\n$lines = explode(\"\\n\", strtolower($lines));\n$i = 0;\n\n// Check if the lines array contains duplicates\n$output = array_unique($lines);\n$output = array_filter($output);\n\nif (is_array($output)) {\n    return implode(\"\\n\", $output);\n} else {\n    return $output;\n}"
+snippet: "$lines = $modx->getOption('input', $scriptProperties, $input);\n$file = $modx->getOption('file', $scriptProperties, '');\n\n// Check first if we're dealing with an external file\nif ($file) {\n    $lines = file_get_contents($file);\n}\n\n// Create an array of all lines inside the input\n$lines = explode(\"\\n\", $lines);\n$i = 0;\n\n// Check if the lines array contains duplicates\n$output = array_unique($lines);\n$output = array_filter($output);\n\nif (is_array($output)) {\n    return implode(\"\\n\", $output);\n} else {\n    return $output;\n}"
 properties: 'a:0:{}'
-content: "$lines = $modx->getOption('input', $scriptProperties, $input);\n$file = $modx->getOption('file', $scriptProperties, '');\n\n// Check first if we're dealing with an external file\nif ($file) {\n    $lines = file_get_contents($file);\n}\n\n// Create an array of all lines inside the input\n$lines = explode(\"\\n\", strtolower($lines));\n$i = 0;\n\n// Check if the lines array contains duplicates\n$output = array_unique($lines);\n$output = array_filter($output);\n\nif (is_array($output)) {\n    return implode(\"\\n\", $output);\n} else {\n    return $output;\n}"
+content: "$lines = $modx->getOption('input', $scriptProperties, $input);\n$file = $modx->getOption('file', $scriptProperties, '');\n\n// Check first if we're dealing with an external file\nif ($file) {\n    $lines = file_get_contents($file);\n}\n\n// Create an array of all lines inside the input\n$lines = explode(\"\\n\", $lines);\n$i = 0;\n\n// Check if the lines array contains duplicates\n$output = array_unique($lines);\n$output = array_filter($output);\n\nif (is_array($output)) {\n    return implode(\"\\n\", $output);\n} else {\n    return $output;\n}"
 
 -----
 
@@ -18,7 +18,7 @@ if ($file) {
 }
 
 // Create an array of all lines inside the input
-$lines = explode("\n", strtolower($lines));
+$lines = explode("\n", $lines);
 $i = 0;
 
 // Check if the lines array contains duplicates
