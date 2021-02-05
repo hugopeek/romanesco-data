0bdd51c85c716383d7cc0487e6155d22fdbe48dc
Hugo Peek <hugo@fractal-farming.com>
2021-02-05 23:05:45 +0800

    Update pattern library

--- snippets/returnFirstHit.php
+++ snippets/returnFirstHit.php
@@ -2,9 +2,9 @@ id: 149
 name: returnFirstHit
 description: 'Feed it a bunch of properties and it spits out the first one that''s not empty. Property names are irrelevant. Sort order is all that matters.'
 category: f_basic
-snippet: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties and it spits out the first one that's not empty.\n * Property names are irrelevant. Sort order is all that matters.\n *\n * [[!returnFirstHit?\n *     &1=`[[+redirect_id]]`\n *     &2=`[[+next_step]]`\n *     &3=`[[*fb_redirect_dynamic]]`\n *     &4=`[[*fb_redirect_id]]`\n *     &default=`Nothing there!`\n * ]]\n */\n\nforeach ($scriptProperties as $property) {\n    if ($property) return $property;\n}\nreturn '';"
+snippet: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties and it spits out the first one that's not empty.\n * Property names are irrelevant. Sort order is all that matters.\n *\n * [[!returnFirstHit?\n *     &1=`[[+redirect_id]]`\n *     &2=`[[+next_step]]`\n *     &3=`[[*fb_redirect_dynamic]]`\n *     &4=`[[*fb_redirect_id]]`\n *     &default=`Nothing there!`\n * ]]\n *\n * @var array $scriptProperties\n */\n\nforeach ($scriptProperties as $key => $value) {\n    if ($key == 'elementExample' || $key == 'elementStatus') continue; // skip snippet properties\n    if ($value) return $value;\n}\nreturn '';"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:38:"romanesco.returnfirsthit.elementStatus";s:4:"type";s:9:"textfield";s:7:"options";s:0:"";s:5:"value";s:5:"solid";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:39:"romanesco.returnfirsthit.elementExample";s:4:"type";s:9:"textfield";s:7:"options";s:0:"";s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
-content: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties and it spits out the first one that's not empty.\n * Property names are irrelevant. Sort order is all that matters.\n *\n * [[!returnFirstHit?\n *     &1=`[[+redirect_id]]`\n *     &2=`[[+next_step]]`\n *     &3=`[[*fb_redirect_dynamic]]`\n *     &4=`[[*fb_redirect_id]]`\n *     &default=`Nothing there!`\n * ]]\n */\n\nforeach ($scriptProperties as $property) {\n    if ($property) return $property;\n}\nreturn '';"
+content: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties and it spits out the first one that's not empty.\n * Property names are irrelevant. Sort order is all that matters.\n *\n * [[!returnFirstHit?\n *     &1=`[[+redirect_id]]`\n *     &2=`[[+next_step]]`\n *     &3=`[[*fb_redirect_dynamic]]`\n *     &4=`[[*fb_redirect_id]]`\n *     &default=`Nothing there!`\n * ]]\n *\n * @var array $scriptProperties\n */\n\nforeach ($scriptProperties as $key => $value) {\n    if ($key == 'elementExample' || $key == 'elementStatus') continue; // skip snippet properties\n    if ($value) return $value;\n}\nreturn '';"
 
 -----
 
@@ -22,9 +22,12 @@ content: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties a
  *     &4=`[[*fb_redirect_id]]`
  *     &default=`Nothing there!`
  * ]]
+ *
+ * @var array $scriptProperties
  */
 
-foreach ($scriptProperties as $property) {
-    if ($property) return $property;
+foreach ($scriptProperties as $key => $value) {
+    if ($key == 'elementExample' || $key == 'elementStatus') continue; // skip snippet properties
+    if ($value) return $value;
 }
 return '';
