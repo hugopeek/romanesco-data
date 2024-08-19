de1d881446074c820e83e33df51003b0abdc98be
Hugo Peek <hugo@fractal-farming.com>
2024-08-19 10:40:58 +0800

    Update pattern library

--- snippets/setUserAccessLevel.php
+++ snippets/setUserAccessLevel.php
@@ -2,7 +2,7 @@ id: 170
 name: setUserAccessLevel
 description: 'Check if logged-in user is a regular member, or has admin privileges. The corresponding member groups are set via the member_groups_frontend and admin_groups_frontend system settings.'
 category: f_user
-snippet: "/**\n * setUserAccessLevel snippet\n *\n * Check if logged-in user is a regular member, or has admin privileges.\n * The corresponding member groups are set via the member_groups_frontend and\n * admin_groups_frontend system settings.\n *\n * Just to be clear: these are frontend access levels only. They have nothing to\n * do with backend access. Depending on the ACL settings, a user could be admin\n * on frontend, but have no manager privileges at all.\n *\n * @var modX $modx\n * @var array $scriptProperties\n */\n\n$output = $modx->getOption('default', $scriptProperties, 'anonymous');\n\nif ($modx->user->id)\n{\n    // First, check if user is admin\n    $adminGroups = $modx->getOption('romanesco.admin_groups_frontend', $scriptProperties);\n    $adminGroups = array_filter(array_map('trim',explode(',',$adminGroups)));\n    foreach($adminGroups as $group) {\n        if ($modx->user->isMember($group)) {\n            $output = 'admin';\n        }\n    }\n\n    // Next, check regular membership\n    if ($output != 'admin') {\n        $memberGroups = $modx->getOption('romanesco.member_groups_frontend', $scriptProperties);\n        $memberGroups = array_filter(array_map('trim',explode(',',$memberGroups)));\n        foreach($memberGroups as $group) {\n            if ($modx->user->isMember($group)) {\n                $output = 'member';\n            }\n        }\n    }\n}\n\n// Output either to placeholder, or directly\n$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\nif ($toPlaceholder) {\n    $modx->setPlaceholder($toPlaceholder, $output);\n    return '';\n}\nreturn $output;"
+snippet: "/**\n * setUserAccessLevel snippet\n *\n * Check if logged-in user is a regular member, or has admin privileges.\n * The corresponding member groups are set via the member_groups_frontend and\n * admin_groups_frontend system settings.\n *\n * Just to be clear: these are frontend access levels only. They have nothing to\n * do with backend access. Depending on the ACL settings, a user could be admin\n * on frontend, but have no manager privileges at all.\n *\n * @var modX $modx\n * @var array $scriptProperties\n */\n\n$output = $modx->getOption('default', $scriptProperties, 'anonymous');\n\nif (!$modx->getOption('romanesco.set_user_access_level', $scriptProperties)) {\n    $output = '';\n}\nelseif ($modx->user->id) {\n    // First, check if user is admin\n    $adminGroups = $modx->getOption('romanesco.admin_groups_frontend', $scriptProperties);\n    $adminGroups = array_filter(array_map('trim',explode(',',$adminGroups)));\n    foreach($adminGroups as $group) {\n        if ($modx->user->isMember($group)) {\n            $output = 'admin';\n        }\n    }\n\n    // Next, check regular membership\n    if ($output != 'admin') {\n        $memberGroups = $modx->getOption('romanesco.member_groups_frontend', $scriptProperties);\n        $memberGroups = array_filter(array_map('trim',explode(',',$memberGroups)));\n        foreach($memberGroups as $group) {\n            if ($modx->user->isMember($group)) {\n                $output = 'member';\n            }\n        }\n    }\n}\n\n$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\nif ($toPlaceholder) {\n    $modx->setPlaceholder($toPlaceholder, $output);\n    return '';\n}\nreturn $output;"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:42:"romanesco.setuseraccesslevel.elementStatus";s:4:"type";s:9:"textfield";s:7:"options";s:0:"";s:5:"value";s:12:"experimental";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:43:"romanesco.setuseraccesslevel.elementExample";s:4:"type";s:9:"textfield";s:7:"options";s:0:"";s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
 
 -----
@@ -25,8 +25,10 @@ properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:
 
 $output = $modx->getOption('default', $scriptProperties, 'anonymous');
 
-if ($modx->user->id)
-{
+if (!$modx->getOption('romanesco.set_user_access_level', $scriptProperties)) {
+    $output = '';
+}
+elseif ($modx->user->id) {
     // First, check if user is admin
     $adminGroups = $modx->getOption('romanesco.admin_groups_frontend', $scriptProperties);
     $adminGroups = array_filter(array_map('trim',explode(',',$adminGroups)));
@@ -48,7 +50,6 @@ if ($modx->user->id)
     }
 }
 
-// Output either to placeholder, or directly
 $toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
 if ($toPlaceholder) {
     $modx->setPlaceholder($toPlaceholder, $output);
