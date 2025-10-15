8d863eab8b5452c3e457cad78708cf570d1ffaf3
Hugo Peek <hugo@fractal-farming.com>
2025-10-15 18:58:11 +0800

    Update pattern library

--- snippets/isEditor.php
+++ snippets/isEditor.php
@@ -2,7 +2,7 @@ id: 160
 name: isEditor
 description: 'Check if user is logged in to the manager.'
 category: f_user
-snippet: "/**\n * isEditor snippet\n *\n * Check if user is logged in to the manager.\n *\n * @author Mark Hamstra\n *\n * @var modX $modx\n */\n\nif ($modx->user instanceof modUser) {\n    if ($modx->user->hasSessionContext('mgr')) {\n        return true;\n    }\n}\nreturn false;"
+snippet: "/**\n * isEditor snippet\n *\n * Check if user is logged in to the manager.\n *\n * @author Mark Hamstra\n *\n * @var modX $modx\n */\n\nif ($modx->user instanceof MODX\\Revolution\\modUser) {\n    if ($modx->user->hasSessionContext('mgr')) {\n        return true;\n    }\n}\nreturn false;"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:5:"solid";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
 
 -----
@@ -18,7 +18,7 @@ properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:
  * @var modX $modx
  */
 
-if ($modx->user instanceof modUser) {
+if ($modx->user instanceof MODX\Revolution\modUser) {
     if ($modx->user->hasSessionContext('mgr')) {
         return true;
     }
--- snippets/stripForJSON.php
+++ snippets/stripForJSON.php
@@ -2,7 +2,7 @@ id: 150
 name: stripForJSON
 description: 'Prepare the input for being used in JSON. This means escaping backslashes and double quotes.'
 category: f_json
-snippet: "/**\n * stripForJSON\n *\n * Escape backslashes and double quotes.\n *\n * @var modX $modx\n * @var array $scriptProperties\n * @var string $input\n */\n\n$input = $modx->getOption('input', $scriptProperties, $input);\nif ($input == '') { return ''; }\nreturn str_replace('\\n','',json_encode($input));"
+snippet: "/**\n * stripForJSON\n *\n * Escape backslashes and double quotes.\n *\n * @var modX $modx\n * @var array $scriptProperties\n * @var string $input\n */\n\n$input = $modx->getOption('input', $scriptProperties, $input);\nif ($input == '') { return ''; }\n$output = str_replace(['\\n', '\\r'], '', $input);\nreturn json_encode($output);"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:5:"solid";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
 
 -----
@@ -20,4 +20,5 @@ properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:
 
 $input = $modx->getOption('input', $scriptProperties, $input);
 if ($input == '') { return ''; }
-return str_replace('\n','',json_encode($input));
+$output = str_replace(['\n', '\r'], '', $input);
+return json_encode($output);
