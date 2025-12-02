8683242761999edc28430662531bd99f7c630ec5
Hugo Peek <hugo@fractal-farming.com>
2025-12-02 17:19:20 +0800

    Update pattern library

--- snippets/tabsGenerateNav.php
+++ snippets/tabsGenerateNav.php
@@ -2,7 +2,7 @@ id: 84
 name: tabsGenerateNav
 description: 'Generate the tab buttons based on data-heading attribute in the tabs themselves. It basically links every tab button to the correct tab content.'
 category: f_presentation
-snippet: "/**\n * tabsGenerateNav\n *\n * Create tab buttons based on the tab content's HTML.\n * Each content field contains data attributes with the correct text for each heading.\n *\n * Many thanks to @christianseel for the original idea and code.\n *\n * @var modX $modx\n * @var array $scriptProperties\n * @var string $input\n * @var string $options\n */\n\n$input = $modx->getOption('input', $scriptProperties, $input);\n$tpl = $modx->getOption('tpl', $scriptProperties, 'tabsNavItem');\n$tplIcon = $modx->getOption('tplIcon', $scriptProperties, 'tabsNavItemIcon');\n\n$prefix = $modx->getOption('prefix', $scriptProperties, '');\n$placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n$doc = new DOMDocument();\n\n// Set error level to suppress warnings in log over special characters in HTML\n$internalErrors = libxml_use_internal_errors(true);\n\n// Load HTML\n$doc->loadHTML('<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">' . $input);\n\n// Restore error level\n$internalErrors = libxml_use_internal_errors(false);\n\n$divs = $doc->getElementsByTagName('div'); // Little flaky.. If div element changes, navigation breaks.\n\n$tabs = array();\n\n$idx = 1;\nforeach($divs as $div) {\n    if ($div->hasAttribute('data-tab')) {\n        $tabs[$div->getAttribute('data-tab')] = array(\n            'heading' => $div->getAttribute('data-heading'),\n            'level' => $div->getAttribute('data-level'),\n            'subtitle' => $div->getAttribute('data-subtitle'),\n            'icon' => $div->getAttribute('data-icon')\n        );\n    }\n}\n\n$tabHeaders = '';\n\n$idx = 1;\nforeach($tabs as $tab) {\n    if ($tab['icon']) {\n        $tpl = $tplIcon;\n    }\n\n    $properties = array(\n        'idx' => $idx,\n        'heading' => $tab['heading'],\n        'level' => $tab['level'],\n        'subtitle' => $tab['subtitle'],\n        'icon' => $tab['icon'],\n    );\n\n    $tabHeaders .= $modx->getChunk($tpl, $properties);\n    $idx++;\n}\n\n// Return placeholder with idx, so tab menu can be justified\n$modx->toPlaceholder('tabs_total', $idx - 1);\n\n// Output either to placeholder, or directly\nif ($placeholder) {\n    $modx->toPlaceholder('pl', $prefix);\n    $modx->toPlaceholder($placeholder, $tabHeaders, $prefix);\n    return '';\n}\nreturn $tabHeaders;"
+snippet: "/**\n * tabsGenerateNav\n *\n * Create tab buttons based on the tab content's HTML.\n * Each content field contains data attributes with the correct text for each heading.\n *\n * Many thanks to @christianseel for the original idea and code.\n *\n * @var modX $modx\n * @var array $scriptProperties\n * @var string $input\n * @var string $options\n */\n\nif (!class_exists(Wa72\\HtmlPageDom\\HtmlPageCrawler::class)) {\n    $modx->log(modX::LOG_LEVEL_ERROR, '[HtmlPageDom] Class not found!');\n    return;\n}\n\nuse Wa72\\HtmlPageDom\\HtmlPageCrawler;\n\n$input = $modx->getOption('input', $scriptProperties, $input);\n$tpl = $modx->getOption('tpl', $scriptProperties, 'tabsNavLeft');\n$rowTpl = $modx->getOption('rowTpl', $scriptProperties, 'tabsNavItem');\n$tplIcon = $modx->getOption('tplIcon', $scriptProperties, 'tabsNavItemIcon');\n\n$prefix = $modx->getOption('prefix', $scriptProperties, '');\n$uid = $modx->getOption('uid', $scriptProperties, '');\n$placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n$dom = new HtmlPageCrawler($input);\n\n$tabHeaders = $dom->filter('.item')->each(function(HtmlPageCrawler $node) use ($uid, $modx) {\n    $html = $node->getAttribute('data-tab');\n//    $html = str_replace('[[+unique_idx]]', $uid, $html);\n//    $modx->log(modX::LOG_LEVEL_ERROR, $html);\n    return $node->setAttribute('data-tab', $html);\n});\n$tabSegments = $dom->filter('.tab.segment');\n\nreturn $modx->getChunk($tpl, [\n    'menu' => implode('',$tabHeaders),\n    'segments' => $tabSegments . PHP_EOL,\n    'tabs_total' => count($tabHeaders),\n    'uid' => $uid,\n]);"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:5:"solid";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
 
 -----
@@ -22,67 +22,35 @@ properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:
  * @var string $options
  */
 
+if (!class_exists(Wa72\HtmlPageDom\HtmlPageCrawler::class)) {
+    $modx->log(modX::LOG_LEVEL_ERROR, '[HtmlPageDom] Class not found!');
+    return;
+}
+
+use Wa72\HtmlPageDom\HtmlPageCrawler;
+
 $input = $modx->getOption('input', $scriptProperties, $input);
-$tpl = $modx->getOption('tpl', $scriptProperties, 'tabsNavItem');
+$tpl = $modx->getOption('tpl', $scriptProperties, 'tabsNavLeft');
+$rowTpl = $modx->getOption('rowTpl', $scriptProperties, 'tabsNavItem');
 $tplIcon = $modx->getOption('tplIcon', $scriptProperties, 'tabsNavItemIcon');
 
 $prefix = $modx->getOption('prefix', $scriptProperties, '');
+$uid = $modx->getOption('uid', $scriptProperties, '');
 $placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
 
-$doc = new DOMDocument();
+$dom = new HtmlPageCrawler($input);
 
-// Set error level to suppress warnings in log over special characters in HTML
-$internalErrors = libxml_use_internal_errors(true);
+$tabHeaders = $dom->filter('.item')->each(function(HtmlPageCrawler $node) use ($uid, $modx) {
+    $html = $node->getAttribute('data-tab');
+//    $html = str_replace('[[+unique_idx]]', $uid, $html);
+//    $modx->log(modX::LOG_LEVEL_ERROR, $html);
+    return $node->setAttribute('data-tab', $html);
+});
+$tabSegments = $dom->filter('.tab.segment');
 
-// Load HTML
-$doc->loadHTML('<meta http-equiv="content-type" content="text/html; charset=utf-8">' . $input);
-
-// Restore error level
-$internalErrors = libxml_use_internal_errors(false);
-
-$divs = $doc->getElementsByTagName('div'); // Little flaky.. If div element changes, navigation breaks.
-
-$tabs = array();
-
-$idx = 1;
-foreach($divs as $div) {
-    if ($div->hasAttribute('data-tab')) {
-        $tabs[$div->getAttribute('data-tab')] = array(
-            'heading' => $div->getAttribute('data-heading'),
-            'level' => $div->getAttribute('data-level'),
-            'subtitle' => $div->getAttribute('data-subtitle'),
-            'icon' => $div->getAttribute('data-icon')
-        );
-    }
-}
-
-$tabHeaders = '';
-
-$idx = 1;
-foreach($tabs as $tab) {
-    if ($tab['icon']) {
-        $tpl = $tplIcon;
-    }
-
-    $properties = array(
-        'idx' => $idx,
-        'heading' => $tab['heading'],
-        'level' => $tab['level'],
-        'subtitle' => $tab['subtitle'],
-        'icon' => $tab['icon'],
-    );
-
-    $tabHeaders .= $modx->getChunk($tpl, $properties);
-    $idx++;
-}
-
-// Return placeholder with idx, so tab menu can be justified
-$modx->toPlaceholder('tabs_total', $idx - 1);
-
-// Output either to placeholder, or directly
-if ($placeholder) {
-    $modx->toPlaceholder('pl', $prefix);
-    $modx->toPlaceholder($placeholder, $tabHeaders, $prefix);
-    return '';
-}
-return $tabHeaders;
+return $modx->getChunk($tpl, [
+    'menu' => implode('',$tabHeaders),
+    'segments' => $tabSegments . PHP_EOL,
+    'tabs_total' => count($tabHeaders),
+    'uid' => $uid,
+]);
