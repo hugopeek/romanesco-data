31849128c7778cb327e10ded12b16641de044913
Hugo Peek <hugo@fractal-farming.com>
2025-12-08 12:50:24 +0800

    Update pattern library

--- snippets/returnFirstHit.php
+++ snippets/returnFirstHit.php
@@ -2,7 +2,7 @@ id: 149
 name: returnFirstHit
 description: 'Feed it a bunch of properties, and it spits out the first one that''s not empty. Property names are irrelevant. Sort order is all that matters.'
 category: f_basic
-snippet: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties, and it spits out the first one that's not empty.\n * Property names are irrelevant. Sort order is all that matters.\n *\n * [[!returnFirstHit?\n *     &1=`[[+redirect_id]]`\n *     &2=`[[+next_step]]`\n *     &3=`[[*fb_redirect_dynamic]]`\n *     &4=`[[*fb_redirect_id]]`\n *     &default=`Nothing there!`\n * ]]\n *\n * @var array $scriptProperties\n */\n\n// Avoid hitting snippet properties\nunset($scriptProperties['elementExample']);\nunset($scriptProperties['elementStatus']);\n\nforeach ($scriptProperties as $key => $value) {\n    if ($value) return $value;\n}\nreturn '';"
+snippet: "/**\n * returnFirstHit snippet\n *\n * Feed it a bunch of properties, and it spits out the first one that's not empty.\n * Property names are irrelevant. Sort order is all that matters.\n *\n * [[!returnFirstHit?\n *     &1=`[[+redirect_id]]`\n *     &2=`[[+next_step]]`\n *     &3=`[[*fb_redirect_dynamic]]`\n *     &4=`[[*fb_redirect_id]]`\n *     &default=`Nothing there!`\n * ]]\n *\n * @var modX $modx\n * @var array $scriptProperties\n */\n\n$placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n// Avoid hitting snippet properties\nunset($scriptProperties['toPlaceholder']);\nunset($scriptProperties['elementExample']);\nunset($scriptProperties['elementStatus']);\n\n$output = '';\nforeach ($scriptProperties as $key => $value) {\n    if ($value) {\n        $output = $value;\n        break;\n    }\n}\nif ($placeholder) {\n    $modx->setPlaceholder($placeholder, $output);\n    return '';\n}\nreturn $output;"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:5:"solid";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
 
 -----
@@ -22,14 +22,26 @@ properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:
  *     &default=`Nothing there!`
  * ]]
  *
+ * @var modX $modx
  * @var array $scriptProperties
  */
 
+$placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
+
 // Avoid hitting snippet properties
+unset($scriptProperties['toPlaceholder']);
 unset($scriptProperties['elementExample']);
 unset($scriptProperties['elementStatus']);
 
+$output = '';
 foreach ($scriptProperties as $key => $value) {
-    if ($value) return $value;
+    if ($value) {
+        $output = $value;
+        break;
+    }
 }
-return '';
+if ($placeholder) {
+    $modx->setPlaceholder($placeholder, $output);
+    return '';
+}
+return $output;
--- snippets/tabsGenerateNav.php
+++ snippets/tabsGenerateNav.php
@@ -2,7 +2,7 @@ id: 84
 name: tabsGenerateNav
 description: 'Generate the tab buttons based on data-heading attribute in the tabs themselves. It basically links every tab button to the correct tab content.'
 category: f_presentation
-snippet: "/**\n * tabsGenerateNav\n *\n * Create tab buttons based on the tab content's HTML.\n * Each content field contains data attributes with the correct text for each heading.\n *\n * Many thanks to @christianseel for the original idea and code.\n *\n * @var modX $modx\n * @var array $scriptProperties\n * @var string $input\n * @var string $options\n */\n\nif (!class_exists(Wa72\\HtmlPageDom\\HtmlPageCrawler::class)) {\n    $modx->log(modX::LOG_LEVEL_ERROR, '[HtmlPageDom] Class not found!');\n    return;\n}\n\nuse Wa72\\HtmlPageDom\\HtmlPageCrawler;\n\n$input = $modx->getOption('input', $scriptProperties, $input);\n$tpl = $modx->getOption('tpl', $scriptProperties, 'tabsNavLeft');\n$rowTpl = $modx->getOption('rowTpl', $scriptProperties, 'tabsNavItem');\n$tplIcon = $modx->getOption('tplIcon', $scriptProperties, 'tabsNavItemIcon');\n\n$prefix = $modx->getOption('prefix', $scriptProperties, '');\n$uid = $modx->getOption('uid', $scriptProperties, '');\n$placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n$dom = new HtmlPageCrawler($input);\n\n$tabHeaders = $dom->filter('.item')->each(function(HtmlPageCrawler $node) use ($uid, $modx) {\n    $html = $node->getAttribute('data-tab');\n//    $html = str_replace('[[+unique_idx]]', $uid, $html);\n//    $modx->log(modX::LOG_LEVEL_ERROR, $html);\n    return $node->setAttribute('data-tab', $html);\n});\n$tabSegments = $dom->filter('.tab.segment');\n\nreturn $modx->getChunk($tpl, [\n    'menu' => implode('',$tabHeaders),\n    'segments' => $tabSegments . PHP_EOL,\n    'tabs_total' => count($tabHeaders),\n    'uid' => $uid,\n]);"
+snippet: "/**\n * tabsGenerateNav\n *\n * Create tab buttons based on the tab content's HTML.\n * Each content field contains data attributes with the correct text for each heading.\n *\n * Many thanks to @christianseel for the original idea and code.\n *\n * @var modX $modx\n * @var array $scriptProperties\n * @var string $input\n * @var string $options\n */\n\nif (!class_exists(Wa72\\HtmlPageDom\\HtmlPageCrawler::class)) {\n    $modx->log(modX::LOG_LEVEL_ERROR, '[HtmlPageDom] Class not found!');\n    return;\n}\n\nuse Wa72\\HtmlPageDom\\HtmlPageCrawler;\n\n$input = $modx->getOption('input', $scriptProperties, $input);\n$tpl = $modx->getOption('tpl', $scriptProperties, 'tabsNavLeft');\n$rowTpl = $modx->getOption('rowTpl', $scriptProperties, 'tabsNavItem');\n$tplIcon = $modx->getOption('tplIcon', $scriptProperties, 'tabsNavItemIcon');\n\n$prefix = $modx->getOption('prefix', $scriptProperties, '');\n$uid = $modx->getOption('uid', $scriptProperties, '');\n$placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);\n\n$dom = new HtmlPageCrawler($input);\n\n// Only include items with reference to a tab\n$tabHeaders = $dom->filter('.item')->each(function(HtmlPageCrawler $node) use ($uid, $modx) {\n    if (!$node->getAttribute('data-tab')) {\n        return '';\n    }\n    return $node;\n});\n\n// Fetch tab content\n$tabSegments = $dom->filter('.tab.segment');\n\nreturn $modx->getChunk($tpl, [\n    'menu' => implode('',$tabHeaders),\n    'segments' => $tabSegments . PHP_EOL,\n    'tabs_total' => count($tabHeaders),\n    'uid' => $uid,\n]);"
 properties: 'a:2:{s:13:"elementStatus";a:7:{s:4:"name";s:13:"elementStatus";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:5:"solid";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}s:14:"elementExample";a:7:{s:4:"name";s:14:"elementExample";s:4:"desc";s:0:"";s:4:"type";s:9:"textfield";s:7:"options";a:0:{}s:5:"value";s:0:"";s:7:"lexicon";s:20:"romanesco:properties";s:4:"area";s:0:"";}}'
 
 -----
@@ -40,12 +40,15 @@ $placeholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
 
 $dom = new HtmlPageCrawler($input);
 
+// Only include items with reference to a tab
 $tabHeaders = $dom->filter('.item')->each(function(HtmlPageCrawler $node) use ($uid, $modx) {
-    $html = $node->getAttribute('data-tab');
-//    $html = str_replace('[[+unique_idx]]', $uid, $html);
-//    $modx->log(modX::LOG_LEVEL_ERROR, $html);
-    return $node->setAttribute('data-tab', $html);
+    if (!$node->getAttribute('data-tab')) {
+        return '';
+    }
+    return $node;
 });
+
+// Fetch tab content
 $tabSegments = $dom->filter('.tab.segment');
 
 return $modx->getChunk($tpl, [
